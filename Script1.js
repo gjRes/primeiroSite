8ebe936dea491ca2cf2a452bce866830ea985035
gjr <gjresdev@outlook.com>
2022-05-17 15:59:48 -0300

    att 17/05

--- Script1.js
+++ Script1.js
@@ -1,6 +1,9 @@
-console.log(soma(5, 15))
+console.log("Funcionando!");
 
-function soma (x,y) {
-    return x + y;
+const Notas=[10,8,7.5,6];
+
+function media(a,b,c,d){
+return (Notas[0] + Notas[1] + Notas[2] + Notas[3]) / Notas.length
 }
 
+console.log(Math.round(media()));
